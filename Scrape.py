35aaff769c6bdca32afc1c04d48602f71d5fef77
stephenhou <stephen.hou.53@gmail.com>
2017-03-26 01:52:14 -0700

    use beautifulsoup to scrap quotes

--- Scrape.py
+++ Scrape.py
@@ -1,6 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
+
 
 r = requests.get("https://www.brainyquote.com/quotes/topics/topic_positive2")
-data = str(r.text)
-data = data.split('<div id="quotesList" class="new-msnry-grid bqcpx" data-sai-includehighlight="true">')
-print(data[1])
+#data = str(r.text)
+#data = data.split('<div id="quotesList" class="new-msnry-grid bqcpx" data-sai-includehighlight="true">')
+#print(data[1])
+
+html_doc = r.text
+
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+quoteTags = soup.findAll('a', { "class" : "b-qt" })
+
+quotes = []
+for quote in quoteTags:
+    quotes.append(quote.text)
+
+print(quotes[0])
