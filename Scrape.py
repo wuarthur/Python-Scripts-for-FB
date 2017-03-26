c14ab08958f2c0f2a564f217181dc41f02f0734d
stephenhou <stephen.hou.53@gmail.com>
2017-03-26 07:46:22 -0700

    fixed grammar

--- Scrape.py
+++ Scrape.py
@@ -18,11 +18,11 @@ def GetQuotes(Category):
             quotes.append(quote.text)
         pageNum += 1
 
-    phrases=["You seem a little down, so here are some words of advice for you",
+    phrases=["You seem a little down, so here are some words of advice for you:",
              "Things aren't going your way? Well here are some words of advice for you:",
              "Having a bad day? Here is some wisdom from famous people to brighten up your day:",
              "A wise man once said:",
-             "It is easy to feel hope less sometimes, but believe me, "]
+             "It is easy to feel hopeless sometimes, but believe me:"]
 
     comment = phrases[randint(0,4)] + "\n" + "\n" + "\""
     comment += quotes[randint(0, len(quotes)-1)] + "\"" + "\n" + "\n" + "If you need someone to talk to, feel free to message me!"
